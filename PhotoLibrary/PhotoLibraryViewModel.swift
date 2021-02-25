81593a789bc65f0f2e0f95eb9a3ad4a14a69a725
Arthur Giachini <arthur.giachini@acad.pucrs.br>
2021-02-25 13:42:32 -0300

    Adding view model for view controller

---
+++ PhotoLibrary/PhotoLibraryViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoLibraryViewModel.swift
+//  PhotoLibrary
+//
+//  Created by Arthur Giachini on 25/02/21.
+//  Copyright © 2021 Arthur Giachini. All rights reserved.
+//
+
+import Foundation
+
+protocol PhotoLibraryViewModelProtocol {
+    
+}
+
+class PhotoLibraryViewModel: PhotoLibraryViewModelProtocol {
+    
+}
