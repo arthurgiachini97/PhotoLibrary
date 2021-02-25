43faab83e7420961f7dd875bb2b0bc42a49ff500
Arthur Giachini <arthur.giachini@acad.pucrs.br>
2021-02-25 11:56:03 -0300

    Adding implemented custom view

---
+++ PhotoLibrary/PhotoLibraryView.swift
@@ -0,0 +1,62 @@
+//
+//  PhotoLibraryView.swift
+//  PhotoLibrary
+//
+//  Created by Arthur Giachini on 24/02/21.
+//  Copyright © 2021 Arthur Giachini. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+final class PhotoLibraryView: UIView {
+    
+    //MARK: Internal variables
+    
+    lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        return searchBar
+    }()
+    
+    lazy var collectionView: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.itemSize = CGSize(width: 150, height: 150)
+        flowLayout.sectionInset = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+        flowLayout.scrollDirection = .vertical
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .red
+        collectionView.register(PhotoLibraryCell.self, forCellWithReuseIdentifier: PhotoLibraryCell.description())
+        return collectionView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubviews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addSubviews() {
+        addSubview(searchBar)
+        addSubview(collectionView)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: topAnchor),
+            searchBar.leadingAnchor.constraint(equalTo: leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: trailingAnchor),
+            
+            collectionView.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+}
